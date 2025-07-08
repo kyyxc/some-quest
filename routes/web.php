cf2083a25694aa6d08fc02c8a9331e532013a5ee
amayones <mayonedrive1@gmail.com>
2025-07-09 06:53:58 +0700

    Fix CI:2

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@ Route::get('/', function () {
     return Inertia::render('Home');
 })->name('home');
 
-// Route::middleware(['auth', 'verified'])->group(function () {
-//     Route::get('dashboard', function () {
-//         return Inertia::render('dashboard');
-//     })->name('dashboard');
-// });
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
